f728e28a18ef3012f15cc65c09475741afea354b
Krunal Chaudhari <krunal.c@avdevs.com>
2020-12-18 10:07:09 +0530

    New SPM
    new changes with SPM

---
+++ Sources/MyLibrary/MyLibrary.swift
@@ -0,0 +1,5 @@
+struct MyLibrary {
+    var text = "Hello, World!"
+    var alertText = "Are you sure you want to create these Alert?"
+    var customAlert = "how to use custom swift package manager in multiple porject"
+}
---
+++ Sources/MyLibrary/customExtension.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Apple on 16/12/20.
+//
+
+import Foundation
